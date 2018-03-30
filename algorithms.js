05b5123487a0b037c2e17ddbacfae679fd4f3382
Eric Carmona <ericscarmona@yahoo.com>
2018-03-29 20:58:59 -0700

    [bug]

---
+++ algorithms.js
@@ -0,0 +1,24 @@
+// const zeroCount = (num) => {
+//   let binary = num.toString(2);
+//       finalMax = 0;
+//       currentMax
+
+//   for (let i = 0; i < binary.length; i ++) {
+//     currentMax = 0;
+//     while (binary[i] === '0') {
+//       ++currentMax && ++i;
+//     }
+//     if (binary[i] === '1') {
+//       finalMax = Math.max(finalMax, currentMax);
+//     }
+//   }
+//   return finalMax;
+// }
+
+// The time complexity is linear O(n) and the space complexity is constant O(C)
+
+
+// class ObjectTaker (obj) {
+
+
+// }
